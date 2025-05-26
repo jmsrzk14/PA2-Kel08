afbbf51094ee3c4f10d51b85fd35e84d3c37b3ab
jmsrzk14 <jmsrizky@gmail.com>
2025-05-26 09:45:46 +0700

    PA2-Kel08

--- KawalPTN-API/main.go
+++ KawalPTN-API/main.go
@@ -4,6 +4,7 @@ import (
 	"KawalPTN-API/database"
 	"KawalPTN-API/routes"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -15,7 +16,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
+		AllowOrigins:     "http://localhost:5173,https://your-app-name.herokuapp.com",
 		AllowMethods:     "GET,POST,PUT,DELETE",
 		AllowHeaders:     "Content-Type, Authorization, X-Requested-With",
 		AllowCredentials: true,
@@ -25,7 +26,12 @@ func main() {
 
 	routes.Setup(app)
 
-	err := app.Listen("localhost:8000")
+	// Gunakan port dari variabel lingkungan PORT atau default ke 8000
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	err := app.Listen(":" + port)
 	if err != nil {
 		log.Fatal(err)
 	}
